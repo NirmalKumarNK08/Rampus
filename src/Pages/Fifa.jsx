f4760cbc9b535452d17a2af93eb9a518a9870348
NirmalKumarNK08 <73978633+NirmalKumarNK08@users.noreply.github.com>
2024-03-16 15:08:41 +0530

    Update Fifa.jsx

--- src/Pages/Fifa.jsx
+++ src/Pages/Fifa.jsx
@@ -8,27 +8,44 @@ const Fifa = () => {
       <div className='h-auto bg-white text-black bg-opacity-75 rounded-xl shadowContainer w-5/6 mb-20 mt-5 flex flex-col items-center justify-center p-3 pb-8'>
         <h1 className='font-bold mt-5 mb-2 underline text-2xl md:text-3xl'>RULES & REGULATIONS</h1>
         <div className='px-5 text-lg flex flex-col gap-2'>
-          <p className='mt-5'> {"->"} Players will participate as a team of 2.</p>
-          <p> {"->"} The game will be conducted in PS5</p>
-          <p> {"->"} No warm-up or practice Games are permitted once the first tourmament match has begun.</p>
-          <p> {"->"} Players may not use any custom teams or players for this competition.</p>
-         <div>
-            <p className='font-bold mt-5 text-xl'>PAUSING:</p>
+          <div className='mt-5'>
+            <p className='font-bold mt-5 text-xl'>TOURNAMENT PLAY:</p>
          </div>
-          <p>{"->"} A player may utilize the pause feature for no     
-              longer than a minute. When the ball has gone out
-              of play, or penalty has been called, the player 
-              WITH POSSESSION may elect to pause the game.
+          <p>{"->"} Tournament will be single elimination with the tournament draw randomly selected.
+          </p>
+          <p> {"->"} All games will be played on Playstation 5 consoles.</p>
+         <div>
+            <p className='font-bold mt-5 text-xl'>GAMEPLAY:</p>
+         </div>
+          <p>{"->"} Half Duration: 5 minute halves | Golden Goal Overtime, then shootout
           </p>
          <div>
-            <p className='font-bold mt-5 text-xl'>OVERTIME:</p>
+            <p className='font-bold mt-5 text-xl'>GAME SETTINGS:</p>
          </div>
-          <p>{"->"} In the event of an overtime use classic mode. 
-          It at the end of classic mode over time (which goes until the 120th minute) 
-          the game is still tied, you then restart the match now playing golden goal 
-          rules under the normal default ruleset (first team to score wins). 
-          Continue restarting the match until one team scores and wins according 
-          to golden goal rules.</p>
+          <p>{"->"} All settings will be pre-set by intramural Staff and setting are open to change by staff.</p>
+          <p> {"->"} If a glitch or freeze occurs in the system game will be restartted with the same setting and game situation.</p>
+        </div>
+        <div>
+            <p className='font-bold mt-5 text-xl'>TEAMS:</p>
+         </div>
+          <p>{"->"} Team should be present 15 mins before their scheduled time.</p>
+          <p> {"->"} Participants should be on the same team all tournament long.</p>
+          <p> {"->"} Home team will be chosen by stone, paper & scissors.</p>
+          <p> {"->"} Mercy Rule: If eithe player gains a lead of 6 or more goals in a game, the game is ended with the 
+              player  leading declared the winner. This rule will not be in effect during semi-finals and championship round.
+          </p>
+          <div>
+              <p className='font-bold mt-5 text-xl'>SPORTMANSHIPS:</p>
+           </div>
+              <p>{"->"} The Intramural Program encourages fair and honourable play bettwe its parrticipants and spectators. If there is an issue of fairness or sportsmanship during your match, contact a memberoof the intramural staaff and explain the situation.
+              </p>
+            </div>
+            <div>
+              <p className='font-bold mt-5 text-xl'>SPORTMANSHIPS:</p>
+               </div>
+                <p>{"->"} Participants who do not show up on start time of their scheduled match have forfeited their opportunity to participate.
+                </p>
+              </div>
         </div>
         <h1 className='font-bold text-2xl md:text-3xl text-center underline mt-8'>EVENT COORDINATORS</h1>
         <div className='flex flex-col justify-center items-center w-full md:flex-row mt-5 gap-5'>
